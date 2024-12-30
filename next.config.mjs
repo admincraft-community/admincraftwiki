56a7d1175e6c5b9f2a6bafc092569a6397a2f7e7
Blstmo <info@blstmo.com>
2024-12-30 23:51:26 +1000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,20 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  webpack: (config, { dev }) => {
+    if (!dev) {
+      config.optimization = {
+        ...config.optimization,
+        minimize: true,
+        splitChunks: {
+          chunks: 'all',
+          minSize: 20000,
+          maxSize: 24000000,
+        }
+      }
+    }
+    return config
+  }
 };
 
 export default withMDX(config);
