111eba63380dbb25aad839cdcc2b80744f98695f
Blstmo <info@blstmo.com>
2024-12-31 00:14:32 +1000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,43 +9,17 @@ const config = {
     if (!dev) {
       config.optimization = {
         ...config.optimization,
-        moduleIds: 'deterministic',
-        chunkIds: 'deterministic',
         splitChunks: {
           chunks: 'all',
           maxInitialRequests: 25,
           minSize: 20000,
-          maxSize: 20000000, 
+          maxSize: 20000000, // 20MB
           cacheGroups: {
-            default: false,
-            vendors: false,
-            framework: {
-              chunks: 'all',
-              name: 'framework',
-              test: /(?<!node_modules.*)[\\/]node_modules[\\/](react|react-dom|scheduler|next|@next)[\\/]/,
-              priority: 40,
-              enforce: true,
-            },
-            lib: {
+            vendor: {
               test: /[\\/]node_modules[\\/]/,
+              name: 'vendors',
               chunks: 'all',
-              name(module, chunks) {
-                return `lib-${chunks[0].name}`
-              },
-              priority: 30,
-            },
-            commons: {
-              name: 'commons',
-              minChunks: 2,
-              priority: 20,
-            },
-            shared: {
-              name(chunks) {
-                return `shared-${chunks[0].name}`
-              },
               priority: 10,
-              minChunks: 2,
-              reuseExistingChunk: true,
             },
           },
         },
