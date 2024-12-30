d8a0a79031062847f718e0be817945369220a8fb
Blstmo <info@blstmo.com>
2024-12-31 00:31:10 +1000

    PK

--- next.config.mjs
+++ next.config.mjs
@@ -5,31 +5,6 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  webpack: (config, { dev, isServer }) => {
-    if (!dev) {
-      // Only apply code splitting on client-side bundles
-      if (!isServer) {
-        config.optimization = {
-          ...config.optimization,
-          splitChunks: {
-            chunks: 'all',
-            maxInitialRequests: 25,
-            minSize: 20000,
-            maxSize: 20000000, // 20MB
-            cacheGroups: {
-              vendor: {
-                test: /node_modules/,
-                name: 'vendor',
-                priority: 10,
-                enforce: true,
-              },
-            },
-          },
-        };
-      }
-    }
-    return config;
-  },
 };
 
 export default withMDX(config);
