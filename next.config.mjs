205d48321da105d7bba513b37c1627c07941ebc7
Blstmo <info@blstmo.com>
2024-12-31 00:10:42 +1000

    DPLU

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,40 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  webpack: (config, { dev}) => {
+    if (!dev) {
+      config.optimization = {
+        ...config.optimization,
+        moduleIds: 'deterministic',
+        chunkIds: 'deterministic',
+        splitChunks: {
+          chunks: 'all',
+          minSize: 20000,
+          maxSize: 20000000, // 20MB, safely under Cloudflare's 25MB limit
+          cacheGroups: {
+            vendor: {
+              test: /[\\/]node_modules[\\/]/,
+              name(module) {
+                // get the name. E.g. node_modules/packageName/not/this/part.js
+                // or node_modules/packageName
+                const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+                // npm package names are URL-safe, but some servers don't like @ symbols
+                return `vendor.${packageName.replace('@', '')}`;
+              },
+              priority: 10,
+              reuseExistingChunk: true,
+            },
+            common: {
+              minChunks: 2,
+              priority: -10,
+              reuseExistingChunk: true,
+            },
+          },
+        },
+      };
+    }
+    return config;
+  },
 };
 
 export default withMDX(config);
