26e72cd15af93543b08eebefa5f1a7955417dfa0
Blstmo <info@blstmo.com>
2024-12-31 00:10:56 +1000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,16 +9,36 @@ const config = {
     if (!dev) {
       config.optimization = {
         ...config.optimization,
-        minimize: true,
+        moduleIds: 'deterministic',
+        chunkIds: 'deterministic',
         splitChunks: {
           chunks: 'all',
           minSize: 20000,
-          maxSize: 24000000,
-        }
-      }
+          maxSize: 20000000, 
+          cacheGroups: {
+            vendor: {
+              test: /[\\/]node_modules[\\/]/,
+              name(module) {
+                // get the name. E.g. node_modules/packageName/not/this/part.js
+                // or node_modules/packageName
+                const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+                // npm package names are URL-safe, but some servers don't like @ symbols
+                return `vendor.${packageName.replace('@', '')}`;
+              },
+              priority: 10,
+              reuseExistingChunk: true,
+            },
+            common: {
+              minChunks: 2,
+              priority: -10,
+              reuseExistingChunk: true,
+            },
+          },
+        },
+      };
     }
-    return config
-  }
+    return config;
+  },
 };
 
 export default withMDX(config);
